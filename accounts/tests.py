70cecdfb4582253052fa638900634766f28d18b1
salih <ahmad.72121@gmail.com>
2023-08-28 17:29:11 +0300

    ch6

--- accounts/tests.py
+++ accounts/tests.py
@@ -1,6 +1,9 @@
 from django.test import TestCase
 from  django.contrib.auth import get_user_model
 from django.urls import reverse
+from .forms import CustomUserCreationForm
+from .views import SignupPageView
+from django.urls import resolve
 
 class CustomUserTests(TestCase):
     def test_create_user(self):
@@ -27,6 +30,25 @@ class CustomUserTests(TestCase):
         self.assertTrue(admin_user.is_active)
         self.assertTrue(admin_user.is_staff)    
         self.assertTrue(admin_user.is_superuser)
+
+
+class SignupPageTests(TestCase):
+    def setUp(self):
+        url  = reverse('signup')
+        self.response = self.client.get(url)
+    def test_signup_template(self):
+        self.assertEqual(self.response.status_code, 200)
+        self.assertTemplateUsed(self.response, 'registration/signup.html')
+        self.assertContains(self.response,'Sign Up')
+    def test_signup_form(self):
+         form = self.response.context.get('form')
+         self.assertIsInstance(form, CustomUserCreationForm)
+         self.assertContains(self.response, 'csrfmiddlewaretoken')
+    def test_signup_view(self): # new
+        view = resolve("/accounts/signup/")
+        self.assertEqual(view.func.__name__, SignupPageView.as_view().__name__)
+
+
 
 
 
--- django_project/settings.py
+++ django_project/settings.py
@@ -136,4 +136,6 @@ STATIC_URL = "static/"
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 AUTH_USER_MODEL = "accounts.CustomUser" # new
+LOGIN_REDIRECT_URL = "home"
+LOGOUT_REDIRECT_URL = "home"
 
