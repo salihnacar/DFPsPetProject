0cd8adeeaf78be7b7019287ff4429ab1ab9fd85c
salih <ahmad.72121@gmail.com>
2023-08-31 16:40:30 +0300

    ch7

--- django_project/settings.py
+++ django_project/settings.py
@@ -39,6 +39,8 @@ INSTALLED_APPS = [
     "django.contrib.staticfiles",
     "accounts.apps.AccountsConfig", # new
     "pages.apps.PagesConfig", # new
+    "crispy_bootstrap5", # new
+    "crispy_forms", # new
    
 ]
 
@@ -130,6 +132,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = "static/"
+STATICFILES_DIRS = [BASE_DIR / "static"] 
+STATIC_ROOT = BASE_DIR / "staticfiles" 
+STATICFILES_STORAGE = "django.contrib.staticfiles.storage.StaticFilesStorage" 
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
@@ -139,3 +144,5 @@ AUTH_USER_MODEL = "accounts.CustomUser" # new
 LOGIN_REDIRECT_URL = "home"
 LOGOUT_REDIRECT_URL = "home"
 
+CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap5" 
+CRISPY_TEMPLATE_PACK = "bootstrap5"
--- pages/tests.py
+++ pages/tests.py
@@ -1,6 +1,6 @@
 from django.test import SimpleTestCase
 from django.urls import reverse, resolve # new
-from .views import HomePageView # new
+from .views import HomePageView, AboutPageView # new
 
 
 class HomepageTests(SimpleTestCase):
@@ -18,5 +18,20 @@ class HomepageTests(SimpleTestCase):
     def test_homepage_url_resolves_homepageview(self): # new
         view = resolve("/")
         self.assertEqual(view.func.__name__, HomePageView.as_view().__name__)
+
+class AboutPageTests(SimpleTestCase):
+    def setUp(self) -> None:
+        url = reverse("about")
+        self.response = self.client.get(url)
+    def test_aboutpage_status_code(self):
+        self.assertEqual(self.response.status_code, 200)
+    def test_aboutpage_template(self):
+        self.assertTemplateUsed(self.response, "about.html")
+    def test_about_page_contains_right_html(self):
+        self.assertContains(self.response, "about page.")
+    def test_aboutpage_resolve_aboutpageview(self):
+        view = resolve("/about/")
+        self.assertEqual(view.func.__name__, AboutPageView.as_view().__name__)
+
         
     
